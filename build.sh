51f32bf83d5afeb94fcf84ff245eaedff90700b1
vincex <dsdvincent@gmail.com>
2024-12-21 22:18:19 -0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #docker-compose build --no-cache album-ai && docker-compose up -d album-ai
-docker-compose build --no-cache postgres album-ai && docker-compose up -d postgres album-ai
+docker compose build --no-cache postgres album-ai && docker compose up -d postgres album-ai
